a328c21310047dfa2f76921e5001619d230b2d02
K.Bailey <katebailey1230@gmail.com>
2017-04-04 15:49:38 -0400

    checkpoint-4

--- app/scripts/Controllers/RoomCtrl.js
+++ app/scripts/Controllers/RoomCtrl.js
@@ -1,17 +1,25 @@
 (function() {
-  function RoomCtrl(Room, $uibModal){
-     this.rooms = Room.all;
-         this.addRoom = function() {
-             $uibModal.open({
-                 templateUrl: '/templates/newroom.html',
-                 size: 'sm',
-                 controller: 'ModalCtrl as modal'
-             });
-         }
-      }
+  function RoomCtrl(Room, Message, $uibModal){
+    var home = this;
+    this.rooms = Room.all;
+    this.currentRoom = null;
+    
+    this.addRoom = function() {
+        $uibModal.open({
+          templateUrl: '/templates/newroom.html',
+          size: 'sm',
+          controller: 'ModalCtrl as modal'
+        });
+       }
   
+    this.setCurrentRoom = function(room){
+      this.currentRoom = room;
+      this.messages = Message.getByRoomId(this.currentRoom.$id);
+    };
+    
+  };
   angular
     .module('blocChat')
-    .controller('RoomCtrl', ['Room','$uibModal', RoomCtrl]);
+    .controller('RoomCtrl', ['Room', 'Message', '$uibModal', RoomCtrl]);
 
 })();
