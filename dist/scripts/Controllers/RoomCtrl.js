d8012346ac4249974991170de2d70a70a9887ae4
K.Bailey <katebailey1230@gmail.com>
2017-04-07 16:26:00 -0400

    checkpoint-6

--- dist/scripts/Controllers/RoomCtrl.js
+++ dist/scripts/Controllers/RoomCtrl.js
@@ -3,6 +3,7 @@
     var home = this;
     this.rooms = Room.all;
     this.currentRoom = null;
+    this.currentUser = $cookies.get('blocChatCurrentUser');
     
     this.addRoom = function() {
         $uibModal.open({
@@ -16,8 +17,14 @@
       this.currentRoom = room;
       this.messages = Message.getByRoomId(this.currentRoom.$id);
     };
+   
+    this.sendMessage = function () {
+            this.newMessage.roomId = this.currentRoom.$id;
+            this.newMessage.username = this.currentUser;
+            Message.send(this.newMessage);
+    }
     
-  };
+  }
   angular
     .module('blocChat')
     .controller('RoomCtrl', ['Room', 'Message', '$uibModal', '$cookies', RoomCtrl]);
